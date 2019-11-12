fdece6f252cdc7ef1a0c302185f9655fc84a85a5
Kat <kat.ochmanska@gmail.com>
2019-11-12 12:55:18 +0100

    added repos

--- src/main/java/com/kat/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/kat/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.kat.recipe.repositories;
 import com.kat.recipe.model.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
