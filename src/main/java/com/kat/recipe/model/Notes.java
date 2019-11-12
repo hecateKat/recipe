3e12a5887158c18fdad59ab2d31fd7e03aaddf04
Kat <kat.ochmanska@gmail.com>
2019-11-12 15:38:41 +0100

    added more lombok annotations

--- src/main/java/com/kat/recipe/model/Notes.java
+++ src/main/java/com/kat/recipe/model/Notes.java
@@ -1,10 +1,12 @@
 package com.kat.recipe.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 
 @Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
