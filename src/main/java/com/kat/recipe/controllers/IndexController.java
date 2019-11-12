711e7a935d996b8935389c3f6aa9e613610690f1
Kat <kat.ochmanska@gmail.com>
2019-11-12 15:34:06 +0100

    added lombok and bootstrap

--- src/main/java/com/kat/recipe/controllers/IndexController.java
+++ src/main/java/com/kat/recipe/controllers/IndexController.java
@@ -3,31 +3,33 @@ package com.kat.recipe.controllers;
 import com.kat.recipe.model.Category;
 import com.kat.recipe.model.UnitOfMeasure;
 import com.kat.recipe.repositories.CategoryRepository;
+import com.kat.recipe.repositories.RecipeRepository;
 import com.kat.recipe.repositories.UnitOfMeasureRepository;
+import com.kat.recipe.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
+    public String getIndexPage(Model model){
 
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional =  unitOfMeasureRepository.findByDescription("Teaspoon");
+        log.debug("Getting Index Page");
 
-        System.out.println("Category Id is " + categoryOptional.get().getId());
-        System.out.println("Unit of measure is " + unitOfMeasureOptional.get().getDescription());
+        model.addAttribute("recipes", recipeService.getRecipes());
+
 
         return "index";
     }
---
+++ src/main/java/com/kat/recipe/services/RecipeService.java
@@ -0,0 +1,12 @@
+package com.kat.recipe.services;
+
+import com.kat.recipe.model.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService {
+
+
+    Set<Recipe> getRecipes();
+
+}
